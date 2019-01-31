57fe71e2811424cac556dca1c9bdbbc81a365dc0
Arylo Yueng <arylo.open@gmail.com>
2019-01-31 11:14:55 +0800

    release(1.1.1): update static fns

--- lib/static.ts
+++ lib/static.ts
@@ -1,23 +1,23 @@
 import { dtss, levelStr } from "./dtss";
 
-export const ms = (time: number, baseLevel: levelStr = "ms") => {
+export function ms(time: number, baseLevel: levelStr = "ms") {
     return new dtss(baseLevel).ms(time);
-};
-export const s = (time: number, baseLevel: levelStr = "ms") => {
+}
+export function s(time: number, baseLevel: levelStr = "ms") {
     return new dtss(baseLevel).s(time);
-};
-export const m = (time: number, baseLevel: levelStr = "ms") => {
+}
+export function m(time: number, baseLevel: levelStr = "ms") {
     return new dtss(baseLevel).m(time);
-};
-export const h = (time: number, baseLevel: levelStr = "ms") => {
+}
+export function h(time: number, baseLevel: levelStr = "ms") {
     return new dtss(baseLevel).h(time);
-};
-export const d = (time: number, baseLevel: levelStr = "ms") => {
+}
+export function d(time: number, baseLevel: levelStr = "ms") {
     return new dtss(baseLevel).d(time);
-};
-export const M = (time: number, baseLevel: levelStr = "ms") => {
+}
+export function M(time: number, baseLevel: levelStr = "ms") {
     return new dtss(baseLevel).M(time);
-};
-export const y = (time: number, baseLevel: levelStr = "ms") => {
+}
+export function y(time: number, baseLevel: levelStr = "ms") {
     return new dtss(baseLevel).y(time);
-};
+}
